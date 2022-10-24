a611a64ad20e540735c5221b5f6e5c08559c05db
Mac <mac@Mac.local>
2022-10-24 14:58:57 +0200

    making some changes on home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,11 @@ export default function Home() {
   console.warn(data.productData);
   return (
     <>
-      <h1 className='text-center mt-3'>All Items</h1>
+      <h1 className='text-center mt-5  '>All Items</h1>
       <section className='py-4 container'>
         <div className='row justify-content-center'>
           {data.productData.map((item, index) => {
-            return (
-              <Itemcard
-                items={item}
-                key={index}
-              />
-            );
+            return <Itemcard items={item} key={index} />;
           })}
         </div>
       </section>
