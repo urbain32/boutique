947be8ab06ae9809e6c6d3d2929bdc6d36b1480d
Mac <mac@Mac.local>
2022-10-24 10:23:49 +0200

    maping through the data and new picture

--- src/component/Itemcard.js
+++ src/component/Itemcard.js
@@ -1,13 +1,18 @@
-export default function Itemcard(props) {
-    const {img,title,desc} = props
+import { useCart } from 'react-use-cart';
+export default function Itemcard({items}) {
+  const { img, title, desc, price } = items;
+  const { addItem } = useCart();
   return (
     <div className='col-11 col-md-6 col-lg-3 mx-0 mb-4'>
       <div className='card p-0 overflow-hidden h-100 shadow'>
-        <img src={img} className='card-img-top img-fluid' alt="" />
-        <div className='card-body'>
+        <img src={img} className='card-img-top img-fluid' alt='' />
+        <div className='card-body text-center'>
           <h5 className='card-title'>{title}</h5>
+          <h5 className='card-title'>FBU {price}</h5>
           <p className='card-text'>{desc}</p>
-          <button className='btn btn-success'>Add to Cart</button>
+          <button className='btn btn-success' onClick={()=>addItem(items)}>
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,20 @@
-import Itemcard from "../component/Itemcard";
-import data from '../data'
+import Itemcard from '../component/Itemcard';
+import data from '../data';
 export default function Home() {
-console.warn(data.productData)
+  console.warn(data.productData);
   return (
     <>
       <h1 className='text-center mt-3'>All Items</h1>
       <section className='py-4 container'>
         <div className='row justify-content-center'>
-          <Itemcard img='img' title='title' desc='desc' />
+          {data.productData.map((item, index) => {
+            return (
+              <Itemcard
+                items={item}
+                key={index}
+              />
+            );
+          })}
         </div>
       </section>
     </>
