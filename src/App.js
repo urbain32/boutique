ee58d16d52b6805499d45e98fb0ab871f2739a8d
Mac <mac@Mac.local>
2022-10-24 14:55:29 +0200

    adding react router dom and icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { CartProvider } from 'react-use-cart';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import Navbar from './component/Navbar';
 import Cart from './pages/Cart';
 import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Order from './pages/Order';
 function App() {
   return (
     <>
-      <CartProvider>
-        <Home />
-        <Cart />
-      </CartProvider>
+      <BrowserRouter>
+        <CartProvider>
+          <Navbar />
+          <div className='container mt-5'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/order' element={<Order />} />
+              <Route path='/cart' element={<Cart />} />
+            </Routes>
+            
+          </div>
+        </CartProvider>
+      </BrowserRouter>
     </>
   );
 }
